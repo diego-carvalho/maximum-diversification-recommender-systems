f3892a00bc50f4aa59e94ffdf78ad4ee5e9b850c
Diego Carvalho <diegocarvalho426@gmail.com>
2018-10-29 15:22:33 -0300

    add recommendations lists

---
+++ Recommendations-Lists/changeFileFormat.sh
@@ -0,0 +1,67 @@
+sed 's/\t/ /g' rec_WRMF.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF.txt
+
+sed 's/\]//g' rec_WRMF.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF.txt
+
+sed 's/\[//g' rec_WRMF.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF.txt
+
+sed 's/:0.[0-9]\+,/:/g' rec_WRMF.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF.txt
+
+sed 's/:0.[0-9]\+//g' rec_WRMF.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF.txt
+
+
+
+sed 's/\t/ /g' rec_WRMF_5.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_5.txt
+
+sed 's/\]//g' rec_WRMF_5.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_5.txt
+
+sed 's/\[//g' rec_WRMF_5.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_5.txt
+
+sed 's/:0.[0-9]\+,/:/g' rec_WRMF_5.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_5.txt
+
+sed 's/:0.[0-9]\+//g' rec_WRMF_5.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_5.txt
+
+
+
+sed 's/\t/ /g' rec_WRMF_10.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_10.txt
+
+sed 's/\]//g' rec_WRMF_10.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_10.txt
+
+sed 's/\[//g' rec_WRMF_10.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_10.txt
+
+sed 's/:0.[0-9]\+,/:/g' rec_WRMF_10.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_10.txt
+
+sed 's/:0.[0-9]\+//g' rec_WRMF_10.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_10.txt
+
+
+
+sed 's/\t/ /g' rec_WRMF_20.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_20.txt
+
+sed 's/\]//g' rec_WRMF_20.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_20.txt
+
+sed 's/\[//g' rec_WRMF_20.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_20.txt
+
+sed 's/:0.[0-9]\+,/:/g' rec_WRMF_20.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_20.txt
+
+sed 's/:0.[0-9]\+//g' rec_WRMF_20.txt > rec_itemKNN_tmp.txt
+mv rec_itemKNN_tmp.txt rec_WRMF_20.txt
+
+
